f55b194802e0fc472b271b26ea0590e14c4b3d1c
infazorrilla <infazorrilla@HZ076028.elorrieta.int>
2023-01-27 15:35:38 +0100

    Primera modificacion

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Estamos empezando el proyecto con Git");
-
+		System.out.println("1ª modificacion del proyecto");
 	}
 
 }
