f4c46f90ed9dc2eaa4b55a2c7c33e816ba036526
vaain <vaain@DESKTOP-E7E5TPS>
2023-01-27 16:25:31 +0100

    Modificación de Ainhoa

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Estamos empezando el proyecto con Git");
 		System.out.println("1ª modificacion del proyecto");
+		System.out.println("2ª modificacion en proyecto de la profesora Ana.");
 	}
 
 }
